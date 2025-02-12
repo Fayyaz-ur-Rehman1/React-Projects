f21d79e347d1dce3aad792ee9d522fe5e5141c9f
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2025-02-13 01:22:08 +0530

    Add-New-Feature

--- manage-contacts/src/App.js
+++ manage-contacts/src/App.js
@@ -1,19 +1,37 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import AddContact from './Components/AddContact';
 import ContactList from './Components/ContactList';
 import Header from './Components/Header';
+import uuid4 from "uuid4"
 
 function App() {
-  const [contact, setContact] = useState([]);
+  const localStorageKey = "Contact"
+  const [contact, setContact] = useState(() => {
+    return JSON.parse(localStorage.getItem(localStorageKey))
+      || []
+  });
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(contact))
+  }, [contact])
+
   const AddUser = (data) => {
-    setContact([...contact, data]);
+    setContact([...contact, { id: uuid4(), data }]);
   }
+
+  const removeContact = (id) => {
+    const updateList = contact.filter((elm) => {
+      return elm.id !== id
+    })
+    setContact(updateList)
+  }
+
   return (
     <div className="App">
       <Header />
       <AddContact AddUser={AddUser} />
-      <ContactList contact={contact} />
+      <ContactList contact={contact} removeContact={removeContact} />
     </div>
   );
 }
--- manage-contacts/src/Components/ContactList.jsx
+++ manage-contacts/src/Components/ContactList.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import DeleteIcon from '@mui/icons-material/Delete';
 
 
-export default function ContactList({ contact }) {
+export default function ContactList(props) {
+    const { contact, removeContact } = props
     const contactList = contact.map(val => {
         return (
             <div className='contacts'>
-                <div className='name'>{val.Name}</div>
-                <div className='email'> {val.Email}</div>
-                <span><DeleteIcon /></span>
+                <div className='name'>{val.data.Name}</div>
+                <div className='email'> {val.data.Email}</div>
+                <span onClick={() => removeContact(val.id)}><DeleteIcon /></span>
             </div>
         )
     })
